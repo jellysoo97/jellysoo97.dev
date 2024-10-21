e85c85b04b12a2736c6020f6cac37e5529c49655
jellysoo97 <jellysoo97@gmail.com>
2024-10-21 16:40:02 +0900

    refactor: refactor common components

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -4,8 +4,8 @@ import { useEffect, useState } from 'react'
 
 import useSwitchTheme from '@/hooks/use-switch-theme'
 
-import ThemeIcon from '../icons/ThemeIcon'
-import IconButton from './IconButton'
+import ThemeIcon from './icons/ThemeIcon'
+import IconButton from './common/IconButton'
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState<boolean>(false)
