151bb8a4b9aedb5b52b118e4933c500473c48037
jellysoo97 <jellysoo97@gmail.com>
2024-10-21 16:39:28 +0900

    chore: change blog title

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,14 +1,13 @@
 export const siteConfig = {
-  title: 'jellysoo97.wiki',
-  description: '배운 것을 정리하고, 인사이트를 공유하는 공간입니다.',
+  title: 'jellysoo97.dev',
+  description: '배운 지식과 생각을 정리하는 공간입니다.',
   author: {
     name: 'jellysoo97',
-    profile: '/images/about/profile.png',
     email: 'jellysoo97@gmail.com',
   },
   url: {
     author: 'https://github.com/jellysoo97',
-    github: 'https://github.com/jellysoo97/jellysoo97-wiki',
-    blog: 'https://jellysoo97-wiki.vercel.app',
+    github: 'https://github.com/jellysoo97/jellysoo97.dev',
+    blog: 'https://jellysoo97.dev.vercel.app',
   },
 }
